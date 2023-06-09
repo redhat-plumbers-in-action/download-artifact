51ef1716f43f28f26a027008e6435c7973b7538b
Jan Macku <jamacku@redhat.com>
2023-06-09 17:24:40 +0200

    WIP: add e2e tests

--- dist/main.js
+++ dist/main.js
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
 import { setFailed } from '@actions/core';
 import { action } from './action';
-try {
-    const message = await action();
-}
-catch (error) {
-    let message;
-    if (error instanceof Error) {
-        message = error.message;
+const run = async (mock) => {
+    try {
+        await action(mock);
     }
-    else {
-        message = JSON.stringify(error);
+    catch (error) {
+        let message;
+        if (error instanceof Error) {
+            message = error.message;
+        }
+        else {
+            message = JSON.stringify(error);
+        }
+        setFailed(message);
     }
-    setFailed(message);
-}
+};
+run();
+export default run;
 //# sourceMappingURL=main.js.map
